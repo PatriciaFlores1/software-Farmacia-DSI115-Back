40927529d6c02bf26387414395956b452873c6b3
PatriciaFlores1 <fb21010@ues.edu.sv>
2025-10-19 00:33:27 -0600

    arreglo de bug en compras en pdf

--- app/Http/Controllers/Purchase/PurchaseController.php
+++ app/Http/Controllers/Purchase/PurchaseController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Purchase;
 use App\Models\Config\Unit;
 use Illuminate\Http\Request;
 use App\Models\Config\Provider;
-use Barryvdh\DomPDF\Facade\pdf;
+use Barryvdh\DomPDF\Facade\Pdf;
 use App\Models\Config\Warehouse;
 use App\Models\Purchase\Purchase;
 use App\Http\Controllers\Controller;
@@ -67,7 +67,8 @@ class PurchaseController extends Controller
     public function sale_pdf($id) {
         $purchase = Purchase::findOrFail($id);
 
-        $pdf = PDF::loadView("purchase.pdf_purchase",compact('purchase'));
+        //$pdf = PDF::loadView("purchase.pdf_purchase",compact('purchase'));
+        $pdf = Pdf::loadView("purchase.pdf_purchase", compact('purchase'));
 
         return $pdf->stream("purchase".$purchase->id.".pdf");
     }
