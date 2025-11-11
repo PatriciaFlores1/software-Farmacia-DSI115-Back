564388a96acd069c532c5f2ef14d10920c6dc83d
CL21005 <cl21005@ues.edu.sv>
2025-11-10 22:34:01 -0600

    cambio variable

--- resources/views/emails/expiration_products.blade.php
+++ resources/views/emails/expiration_products.blade.php
@@ -1,6 +1,6 @@
 <h2>Productos por expirar pronto</h2>
 <ul>
 @foreach($products as $product)
-    <li>{{ $product->title }} - {{ $product->description }} — expira en {{ $product->expiration_days }} dias</li>
+    <li>{{ $product->title }} - {{ $product->description }} — expira en {{ $product->expiration_date }} dias</li>
 @endforeach
 </ul>
